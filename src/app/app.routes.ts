90a624c15533cbbe3ed772ddefaa5302a97ee0fe
Dhony Imam Saputra <dhony.saputra@bizzy.co.id>
2019-05-17 20:06:34 +0700

    change indentation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,17 @@ import { LoginComponent } from './page/login/login.component';
 import { TestProjectComponent } from './page/test-project/test-project.component';
 
 export const routes: Routes = [
-	{
-	    path: '',
-	    component: LoginComponent,
-		  resolve: {
-		    data: CategoriesResolver
+{
+  path: '',
+	component: LoginComponent,
+	resolve: {
+	      data: CategoriesResolver
 			}
-	},
-	{
-	    path: 'questions/about/:categorySlug',
-	    component: TestProjectComponent,
-	    resolve: {
+},
+{
+	path: 'questions/about/:categorySlug',
+	component: TestProjectComponent,
+	resolve: {
 	      data: CategoryQuestionsResolver
 	  	}
 	},
